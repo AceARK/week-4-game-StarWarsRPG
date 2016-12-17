3f71fa1ac048d8542aa8a64cbbf59ec38a2c9662
Ace <Ace>
2016-12-17 01:04:07 -0500

    Pre-debug commit of buggy code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -49,45 +49,97 @@ $(document).ready(function(){
 	var attacker;
 	var defender;
 	var attackerLocked = false;
+	var defenderLocked = false;
+	var attackEnabled = false;
+	var contenderDefeated = 0;
 
-	$(".contender").on("click", function() {
+	$("#lockDefender").prop('disabled', true);
+	$("#attack").prop('disabled', true);
+
+	$(".contender").on("click", function() {	
+		///////////////////////////// BUGGY code //////////////////////////////////
+		// $(this).hide(); // hiding chosen contender from list of contenders
 		if(!attackerLocked) {
+			///////////////////////////// BUGGY code //////////////////////////////////
+			// $(".contender").each(function() {
+			// 	if($(".contender").data("info").name = $(".attacker").data("info").name) {
+			// 		$(".contender").show();
+			// 	}
+			// });
+			///////////////////////////// BUGGY code //////////////////////////////////
 			$(".attacker").html($(this).html());
 			$(".attacker").data("info", $(this).data("info"));
-		} else {
+		} else if(!defenderLocked) {
 			$(".defender").html($(this).html());
-			$(".defender").data("info", $(this).data("info"));
+			$(".defender").data("info", $(this).data("info"));			
 		}
 		     
 	});
 
-	// Creating new attacker instance /////////////////////// On click of Lock attacker button /////////////////////////
+	// Creating new attacker instance 
 	$("#lockAttacker").on("click", function() {
 		attackerLocked = true;
 		var attackerInfo = $(".attacker").data("info");
 		attacker = new Character(attackerInfo.name, attackerInfo.attackPower, attackerInfo.healthPoints,0);
 		$(this).prop('disabled', true);
+		$("#lockDefender").prop('disabled', false);
 	});
 
-	//////////////////// After lock attacker button, listen for .enemy click //////////////////////
 	// Creating new defender instance
 	$("#lockDefender").on("click", function() { 
-		var defenderInfo = $(".defender").data("info");
-		defender = new Character(defenderInfo.name, 0, defenderInfo.healthPoints, defenderInfo.counterAttackPower);
-		$(this).prop('disabled', true);
+		if(attackerLocked) {
+			defenderLocked = true;
+			var defenderInfo = $(".defender").data("info");
+			defender = new Character(defenderInfo.name, 0, defenderInfo.healthPoints, defenderInfo.counterAttackPower);
+			$(this).prop('disabled', true);
+			$("#attack").prop('disabled', false);
+		}
 	});
-
-	////////////////// After lock defender button, enable #attack button /////////////////////
 
 	// Commencing attack sequence on button click
 	$("#attack").on("click", function(){
-		attacker.attack(defender);
-		if(defender.healthPoints <= 0) {
-			console.log("Choose next defender.");
-		}
-		else if(attacker.healthPoints <= 0) {
-			console.log("You lose. Restart game.");
+		if(attackerLocked && defenderLocked) {
+			attackEnabled = true;
+			attacker.attack(defender);
+			if(defender.healthPoints <= 0) {
+				if(contenderDefeated >= 2) {
+					console.log("Contender defeated " + contenderDefeated);
+					console.log("You win. Reset game to start anew.");  
+					$("#attack").prop('disabled', true);
+					attackEnabled = false;
+				} else {
+					contenderDefeated++;
+					console.log("Contender defeated " + contenderDefeated);
+					console.log("Choose next defender.");
+					defenderLocked = false;
+					$("#lockDefender").prop('disabled', false);
+					attackEnabled = false;
+					$("#attack").prop('disabled', true);
+				}
+				
+			}
+			else if(attacker.healthPoints <= 0) {
+				console.log("You lose. Restart game.");
+			}
 		}
 	});
 
+	$("#reset").on("click", function() {
+		reset();
+	});
+
 });
+
+function reset() {
+		var attackerLocked = false;
+		var defenderLocked = false;
+		var attackEnabled = false;
+		var contenderDefeated = 0;
+
+		$(".contender").each(function() {
+			$(this).show();
+		});
+
+		////////////// Yet to complete reset function //////////////////
+		
+	}
